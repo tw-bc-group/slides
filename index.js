70826d6fb52b741ef62d8c61ba82fdc097bb4805
Yan Qian <qianyan@thoughtworks.com>
2018-09-17 08:42:39 +0800

    feat(index.js): convert slides.md to index.html

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require("fs")
 const _ = require("lodash")
-const ex = require("./src/extract_slides.js")
+const extractSlides = require("./src/extract_slides.js")
 const showSlides = require("./src/show_slides.js")
 
-const json = showSlides(ex(fs.readFileSync("./slides.md", {encoding: "UTF-8"})))
-const t = _.template(fs.readFileSync("./index.html", {encoding: "UTF-8"}))
-console.log(t({slides: json.join("\n")}))
+const src = fs.readFileSync("./slides.md", {encoding: "UTF-8"})
+const slides = showSlides(extractSlides(src))
+
+const template = fs.readFileSync("./index.tpl", {encoding: "UTF-8"})
+const t = _.template(template)
+
+const html = t({slides: slides.join("\n")})
+
+fs.writeFileSync("index.html", html, {encoding: "UTF-8"})
